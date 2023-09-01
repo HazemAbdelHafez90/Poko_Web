5b978fd1f8eed09ce8e069d6d1a9490885a7e02e
HazemAbdelHafez90 <128214948+HazemAbdelHafez90@users.noreply.github.com>
2023-09-01 19:36:13 +0300

    Fix bugs
    * removing unneeded components.
    * Add searching on typing.

--- poko_web/src/app/app-routing.module.ts
+++ poko_web/src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookFormComponent } from './book-form/book-form.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [
-  { path: '', component: BooksComponent },
-  { path: 'search', component: BookListComponent },
-  { path: 'add', component: BookFormComponent },
-];
+const routes: Routes = [{ path: '', component: BookFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
